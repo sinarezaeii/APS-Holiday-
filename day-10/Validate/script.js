8b2a457d9bf78100a3a034c3ae246c881c06ca5b
sinarezaeii <sina.cod6610@gmail.com>
2022-04-07 11:06:46 -0700

    yes

--- day-10/Validate/script.js
+++ day-10/Validate/script.js
@@ -3,7 +3,7 @@ let phoneError = document.getElementById("phone-error");
 let emailError = document.getElementById("email-error");
 let messageError = document.getElementById("message-error");
 let submitError = document.getElementById("submit-error");
-
+ 
 function validateName() {
   let name = document.getElementById("contact-name").value;
   if (name.length == 0) {
